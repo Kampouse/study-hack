43e51cdba230ebb8dc44b7a9388ae34609930851
jean-philippe martel <jpmartel98@gmail.com>
2024-07-07 14:24:04 -0400

    looking good maybe

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,10 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 const Landing = component$(() => {
   return (
-    <div class="bg-hero-bg   bg-center px-32  md:h-[110vh] md:w-[68em]  lg:h-screen   lg:w-[80em] ">
+    <div class="bg-hero-bg  h-[80vh]  bg-cover bg-center  px-[25vw] pt-10    md:h-[110vh] md:w-[68em]  lg:h-screen   lg:w-[80em] ">
       <div class="flex h-full flex-col items-center justify-center">
-        <h1 class="self-start text-5xl font-bold text-black md:ml-[3.5em] lg:ml-[5.5em]">
-          Learn & Hack
-        </h1>
-        <p class="w-[22em] px-4   text-2xl text-black ">
+        <h1 class="text-5xl">Learn & Hack</h1>
+        <p class=" w-[12em] text-lg text-black md:w-[17em] md:text-2xl lg:px-5 ">
           {" "}
           have you consider writing an os from scratch but think your too bad?
         </p>
