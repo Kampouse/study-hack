05169758c5fc0de0774f1694348bb96d640c36f0
jean-philippe martel <jpmartel98@gmail.com>
2024-07-07 12:54:59 -0400

    made it look ok md and lg idk about smol screeen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 const Landing = component$(() => {
   return (
-    <div class="bg-hero-bg   bg-center  px-32 md:h-[110vh] md:w-[68em] lg:h-[100vh]  lg:w-[80em]  lg:scale-110">
+    <div class="bg-hero-bg   bg-center px-32  md:h-[110vh] md:w-[68em]  lg:h-screen   lg:w-[80em] ">
       <div class="flex h-full flex-col items-center justify-center">
         <h1 class="self-start text-5xl font-bold text-black md:ml-[3.5em] lg:ml-[5.5em]">
           Learn & Hack
