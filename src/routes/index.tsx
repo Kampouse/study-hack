8fbc0928000cc1a0f38e64e4a6167c663adb988f
jean-philippe <jp@zelexio.com>
2024-07-08 19:04:22 -0400

    working fine maybe

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,11 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 const Landing = component$(() => {
   return (
-    <div class="bg-hero-bg     flex items-center flex-col justify-center py-40   bg-cover h-[33em] bg-center  px-16    md:h-[110vh]  md:w-[68em] lg:px-96 md:px-60  lg:h-screen   lg:w-[80em]  ">
+    <div class="bg-hero-bg     flex items-center flex-col justify-center py-40   bg-cover h-[34em] bg-center  px-16    md:h-[110vh]  md:w-[68em] lg:px-96 md:px-60  lg:h-screen   lg:w-[80em]  ">
       <h1 class="text-3xl md:hidden pb-8    ">Study & Hack</h1>
       <div class=" flex h-full flex-col px-2   lg:pl-10 lg:pt-12  md:mt-10 lg:items-start md:items-center md:pt-8 justify-center gap-2  md:gap-4">
         <h1 class=" hidden md:flex   md:text-5xl    ">Study & Hack</h1>
-        <p class=" max-w-[21em] w-fit pl-4 px-2 text-md self-start font-medium   bg-opacity-30 rounded-xl  text-black  md:text-2xl md:px-24 lg:px-0  ">
+        <p class=" max-w-[15em] md:max-w-[25em]  w-fit pl-4 px-2 text-md self-start font-medium   bg-opacity-30 rounded-xl  text-black  md:text-2xl md:px-24 lg:px-0  ">
           Feeling isolated while working? Connect with a fellow techies in Montreal to study, share ideas, and stay motivated on your cyber journey.
         </p>
         <div class=" flex  md:flex-row gap-4       md:self-start  md:pl-24 lg:pl-0 px-2 md:px-0   ">
