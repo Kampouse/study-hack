31c26f45bca0a97dd7acdab8ed8c8f1e0c6bd37a
jean-philippe <jp@zelexio.com>
2024-07-08 18:56:48 -0400

    working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,14 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 const Landing = component$(() => {
   return (
-    <div class="bg-hero-bg     flex items-center flex-col justify-center py-40   bg-cover h-[80vh] bg-center  px-16    md:h-[110vh]  md:w-[68em] md:px-[25vw]  lg:h-screen   lg:w-[80em] ">
-      <h1 class="text-3xl md:hidden pb-10    ">Study & Hack</h1>
-      <div class="flex h-full flex-col   lg:pl-10 lg:pt-16  md:mt-10 lg:items-start md:items-center md:pt-8 justify-center gap-2  md:gap-4">
+    <div class="bg-hero-bg     flex items-center flex-col justify-center py-40   bg-cover h-[33em] bg-center  px-16    md:h-[110vh]  md:w-[68em] lg:px-96 md:px-60  lg:h-screen   lg:w-[80em]  ">
+      <h1 class="text-3xl md:hidden pb-8    ">Study & Hack</h1>
+      <div class=" flex h-full flex-col px-2   lg:pl-10 lg:pt-12  md:mt-10 lg:items-start md:items-center md:pt-8 justify-center gap-2  md:gap-4">
         <h1 class=" hidden md:flex   md:text-5xl    ">Study & Hack</h1>
-        <p class=" w-fit px-2 text-md self-start font-medium   bg-opacity-30 rounded-xl  text-black  md:text-2xl md:px-24 lg:px-0  ">
+        <p class=" max-w-[21em] w-fit pl-4 px-2 text-md self-start font-medium   bg-opacity-30 rounded-xl  text-black  md:text-2xl md:px-24 lg:px-0  ">
           Feeling isolated while working? Connect with a fellow techies in Montreal to study, share ideas, and stay motivated on your cyber journey.
         </p>
-        <div class=" flex  md:flex-row gap-4      md:self-start  md:pl-24 lg:pl-0 px-2 md:px-0   ">
+        <div class=" flex  md:flex-row gap-4       md:self-start  md:pl-24 lg:pl-0 px-2 md:px-0   ">
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 md:p-2  rounded-lg">
             Get Started
           </button>
