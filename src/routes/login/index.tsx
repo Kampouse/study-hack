f2701faa195462ab405a6ac15f64f6c8527a9ced
jean-philippe <jp@zelexio.com>
2024-07-16 19:07:54 -0400

    fix some issue on user creation and redirection flow

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,6 +1,35 @@
 import { useAuthSignin } from "../plugin@auth";
 import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
+import type { RequestHandler } from "@builder.io/qwik-city";
+export const onGet: RequestHandler = async (req) => {
+
+  type Session = {
+    user: {
+      name: string;
+      email: string;
+      image: string;
+    };
+    expires: string;
+  } | null;
+  const data = req.sharedMap.get("session") as Session;
+  //get current session time and compare with expires time
+  if (data) {
+    const currentTime = new Date().getTime();
+    const sessionDate = new Date(data.expires).getTime();
+    if (currentTime < sessionDate) {
+      throw req.redirect(302, "/app");
+    }
+  }
+
+};
+
+
+
+
+
+
+
 
 export default component$(() => {
   const signin = useAuthSignin();
--- src/routes/login/layout.tsx
+++ src/routes/login/layout.tsx
@@ -1,4 +1,18 @@
 import { component$, Slot } from "@builder.io/qwik";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default component$(() => {
   return (
     <div class="">
