70dbffc3b03ffbf0dcbad32e6aa940527aa9c9a8
jean-philippe <jp@zelexio.com>
2024-07-16 19:13:03 -0400

    remove cache since may be  breaking stuff

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -19,12 +19,12 @@ export const onGet: RequestHandler = async (event) => {
 
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
-  event.cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
+  // event.cacheControl({
+  // Always serve a cached response by default, up to a week stale
+  // staleWhileRevalidate: 60 * 60 * 24 * 7,
+  // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+  //    maxAge: 5,
+  //});
   //if (url.pathname == "/" && session) throw redirect(302, "/app");
 };
 
