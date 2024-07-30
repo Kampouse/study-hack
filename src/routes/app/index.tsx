4f05a35896a9562d92ccf1a184bba81fa1c90e6b
jean-philippe <jp@zelexio.com>
2024-07-29 23:03:02 -0400

    added  content to page

--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -19,10 +19,10 @@ export default component$(() => {
         <h1 class="text-3xl font-medium ">Community</h1>
         <p class="text-md   font-thin  text-black"> See who active this week</p>
       </div>
-      <div class="flex grid-cols-5 flex-col gap-2 md:gap-2 lg:grid">
+      <div class="flex grid-cols-5 flex-col gap-5 md:gap-2 lg:grid">
         <div class="row-span-1 h-fit rounded-full  lg:col-span-3  lg:pl-4 ">
           <Leaflet />
-          <div class=" px-2 pt-2 ">
+          <div class=" px-2 pt-10 pb-4 ">
             <h1 class="py-2 text-2xl font-medium "> Intrests Filters</h1>
             <div class=" flex gap-2 ">
               {intrests.map((intrest) => (
@@ -36,15 +36,24 @@ export default component$(() => {
             </div>
           </div>
           <div class=" px-2 pt-2 ">
-            <h1 class="py-2 text-2xl font-medium ">Good locations</h1>
-            <div class=" flex gap-2 ">
+            <h1 class=" text-2xl font-medium pb-4 ">Good locations</h1>
+            <div class=" flex  flex-col content-center md:flex-row md:gap-40 gap-8 w-full  ">
               {location.map((location) => (
-                <button
-                  key={location}
-                  class=" max-w-lg self-end rounded-md border bg-blue-50 p-2  text-black shadow-sm  "
-                >
-                  {location}
-                </button>
+                <div key={location} class="flex flex-col  min-w-fit md:w-[25em] gap-2  ">
+                  <img src="https://s3.ca-central-1.amazonaws.com/files.quartierdesspectacles.com/lieux/esplanade-tranquille/2022/printemps/esplanade-tranquille-pqds-2022-esplanade-tranquille-printemps-ete-c-vivien-gaumand-20-390x260.jpg" alt="location" width={500} height={100} class=" max-w-screen-md h-full   rounded-md shadow-lg" />
+                  <div class="flex flex-col  gap-2  bg-blue-50 pl-2   bg-opacity-30 align-middle w-full ">
+                    <p class="text-md font-thin    text-black  ">
+
+                      <span class="font-medium"> {location} </span> open 24/7</p>
+                    <p class=" text-md font-thin  text-black ">a cozy spot where people dont bother your for coffee   </p>
+                    <button class=" cursor-pointer max-w-lg self-start rounded-md border bg-blue-50 p-2  text-black shadow-sm  ">
+                      book now
+                    </button>
+
+                  </div>
+
+                </div>
+
               ))}
             </div>
           </div>
