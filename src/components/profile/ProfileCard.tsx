0222717a6ffee533a06fa04201199486b1784c45
jean-philippe <jp@zelexio.com>
2024-08-08 18:35:30 -0400

    change location of name to look better

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -13,12 +13,13 @@ export default component$<CardProps>(({ data }) => {
   const image = session.value?.user?.image;
   return (
     <div class="flex flex-col items-center   gap-5 md:flex-row md:gap-5 lg:items-start ">
-      <div class="flex flex-col justify-center">
+
+      <div class="flex flex-col justify-center ">
         <div
-          class=" flex flex-col  px-2 md:items-start
+          class=" flex flex-col md:flex-row  px-2 md:items-start
         lg:items-start"
         >
-          <div class="">
+          <div class="flex   flex-row">
             <Modal.Trigger>
               <img
                 src={image || "https://via.placeholder.com/200"}
@@ -30,7 +31,7 @@ export default component$<CardProps>(({ data }) => {
 
             <Modal.Trigger
               class="
-                  self-end rounded-full bg-white p-3 text-sm text-black shadow-[0_8px_15px_rgba(0,0,0,0.1)] transition-all hover:bg-[#90EE90] hover:text-white
+                  self-end rounded-full bg-white pl-4 p-3 text-sm text-black shadow-[0_8px_15px_rgba(0,0,0,0.1)] transition-all hover:bg-[#90EE90] hover:text-white
                   "
             >
               <svg
@@ -50,26 +51,27 @@ export default component$<CardProps>(({ data }) => {
                 <path d="m15 5 3 3" />
               </svg>
             </Modal.Trigger>
+          </div>
+          <div class=" self-center">
             <h1 class=" pl-2 py-0 text-3xl text-zinc-900">{data.name}</h1>
             <p class="py-1 pl-2 text-[#939393]">{data.about}</p>
-          </div>
 
-
-          <div class="flex flex-wrap items-start gap-2 ">
-            {data.interests.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  class="w-fit cursor-pointer rounded-lg bg-gray-300 p-2 text-black hover:bg-[#90EE90]"
-                >
-                  {item}
-                </div>
-              );
-            })}
+            <div class="flex flex-wrap items-start gap-2 px-2 ">
+              {data.interests.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    class="w-fit cursor-pointer rounded-lg bg-gray-300 p-2 text-black hover:bg-[#90EE90]"
+                  >
+                    {item}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-      <div class="order-first self-start md:order-last"></div>
     </div>
+
   );
 });
