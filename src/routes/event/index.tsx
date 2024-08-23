52652ecf3cbe2ea94204f3dab90152f51828e2d0
asiill <asiill>
2024-08-23 17:01:18 -0400

    Change form styles

--- src/routes/event/index.tsx
+++ src/routes/event/index.tsx
@@ -13,6 +13,8 @@ const useCreateEvent = routeAction$(async (data, event) => {
 
 export default component$(() => {
 
+  const interests =["JavaScript", "Python", "Stuff", "Plants", "Things"];
+
   const store = useStore({
     name: "",
     description: "",
@@ -26,9 +28,11 @@ export default component$(() => {
   
   const action = useCreateEvent();
   return (
-    <div class="flex flex-col gap-6 rounded-lg bg-white p-6 shadow-[0_8px_15px_rgba(0,0,0,0.1)]">
-      <div>
-        <p class="text-xl text-[#505050]">Make changes to your account here.</p>
+    <div class="flex justify-center p-6">
+    <div class="flex flex-col content-center w-[500px] gap-6 rounded-3xl bg-white p-8 shadow-[0_8px_15px_rgba(0,0,0,0.1)]">
+      <div class="flex flex-col gap-2 items-center">
+        <h2 class="text-3xl">Create your Event</h2>
+        <p class="text-xl text-[#505050]">Add details and create your event</p>
       </div>
       <Form
         onSubmitCompleted$={(e) => {
@@ -38,45 +42,86 @@ export default component$(() => {
         class="flex flex-col gap-4"
       >
         <div class="flex flex-col gap-2">
-          <label>When are you going?</label>
-          <select class="rounded-lg border border-gray-300 bg-white p-3 text-sm text-black focus:border-[#90EE90]">
-            <option>9h</option>
-            <option>10h</option>
-            <option>11h</option>
-            <option>12h</option>
-            <option>13h</option>
-            <option>14h</option>
-            <option>15h</option>
-            <option>16h</option>
-            <option>17h</option>
-            <option>18h</option>
-            <option>19h</option>
-          </select>
-        </div>
-        <div class="flex flex-col gap-2">
-          <label for="what">What are you working on?</label>
+          <label>Event name</label>
           <input
             class="rounded-lg border border-gray-300 bg-white p-3 text-sm text-black outline-none focus:border-[#90EE90]"
             type="text"
-            id="what"
-            name="what"
+            id="name"
+            name="name"
+            placeholder="Just studyin' for fun :/"
+            value={store.name}
+          />
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <label for="what">What will you be working on?</label>
+          <input
+            class="rounded-lg border border-gray-300 bg-white p-3 text-sm text-black outline-none focus:border-[#90EE90]"
+            type="text"
+            id="description"
+            name="description"
+            placeholder="I will be learning about machine learning."
             value={store.description}
           />
         </div>
+
         <div class="flex flex-col gap-2">
           <label>Where will you be?</label>
           <input
             class="rounded-lg border border-gray-300 bg-white p-3 text-sm text-black outline-none focus:border-[#90EE90]"
-            type="text"
-            id="where"
-            name="where"
+            type="search"
+            id="location"
+            name="location"
+            placeholder="Somewhere beyond the sea..."
             value={store.location}
           />
         </div>
-        <button class="w-fit rounded-lg bg-[#90EE90] p-2.5 shadow-[0_8px_15px_rgba(0,0,0,0.1)] hover:opacity-80">
+
+        <div class="flex gap-4 items-center">
+          <div class="flex flex-col gap-2 w-full">
+            <label>Date</label>
+            <input
+              class="rounded-lg border border-gray-300 bg-white p-3 text-sm text-black outline-none focus:border-[#90EE90]"
+              type="date"
+              id="date"
+              name="date"
+              placeholder="03/03/24"
+              value={store.date}
+            />
+          </div>
+          <div class="flex flex-col gap-2 w-full">
+            <label>Time</label>
+            <input
+              class="rounded-lg border border-gray-300 bg-white p-3 text-sm text-black outline-none focus:border-[#90EE90]"
+              type="time"
+              id="time"
+              name="time"
+              placeholder="16h30"
+              value={store.date}
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-2">
+            <label>Select interests</label>
+            <select class="rounded-lg border border-gray-300 bg-white p-3 text-sm text-black focus:border-[#90EE90]">
+              {interests.map((item) => {
+                const id = item.indexOf(item);
+                return (
+                  <option
+                    key={id}
+                  >{item}</option>
+                )
+              })}
+              
+            </select>
+          </div>
+        
+        <button class="rounded-lg bg-[#90EE90] p-2.5 shadow-[0_8px_15px_rgba(0,0,0,0.1)] hover:opacity-80">
           Save
         </button>
       </Form>
+    </div>
     </div>
   );
 });
---
+++ src/routes/event/layout.tsx
@@ -0,0 +1,8 @@
+import { component$, Slot } from "@builder.io/qwik";
+export default component$(() => {
+  return (
+    <div class="">
+      <Slot />
+    </div>
+  );
+});
