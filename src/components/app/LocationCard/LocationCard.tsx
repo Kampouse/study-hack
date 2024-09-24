8603529fdc8a1557a5a8206edeaef0e4fc9f582b
jean-philippe martel <jpmartel98@gmail.com>
2024-09-24 17:38:13 -0400

    modified the location thing on the home page

--- src/components/app/LocationCard/LocationCard.tsx
+++ src/components/app/LocationCard/LocationCard.tsx
@@ -1,54 +1,62 @@
 import { component$ } from "@builder.io/qwik";
 import { Tag } from "./Tag";
-import { AttendeeList } from "./AttendeeList";
-
-export type EventCardProps = {
-  title: string;
+export type LocationCardProps = {
+  name: string;
   description: string;
-  time: string;
+  address: string;
   tags: string[];
-  attendees: number;
+  rating: number;
   link: string;
 };
-const imgs = ["hello", "stuff", "world"];
-export const EventCard = component$((props: EventCardProps) => {
+
+export const LocationCard = component$((props: LocationCardProps) => {
   return (
-    <article class="flex  w-full flex-col overflow-hidden rounded-2xl bg-white p-4 shadow-[0px_4px_12px_rgba(0,0,0,0.04)] lg:w-full">
+    <article class="flex w-full max-w-md flex-col overflow-hidden rounded-2xl bg-white p-4 shadow-[0px_4px_12px_rgba(0,0,0,0.04)] lg:w-full">
       <header class="relative flex aspect-[2.85] w-full flex-col items-center justify-center overflow-hidden rounded-xl px-14 py-10">
         <img
           loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/3f0587d3f7ff9e636884c0c2be54daa3f001d89360b3a629cd9b9bf25b05857c?placeholderIfAbsent=true&apiKey=17e6b1f5aff14ccdbcafd5a66c4951ca"
-          class="size-full absolute inset-0 object-cover"
+          src="https://hellolaroux.com/wp-content/uploads/2022/05/esplanade-tranquille-montreal-quartier-des-spectacles.jpg"
+          class="absolute inset-0 h-full w-full object-cover"
           alt=""
-          width={500}
-          height={500}
+          width={1000}
+          height={1000}
         />
         <img
           loading="lazy"
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/c9d86d9147e5822062e107d2ac807972850acb658678ae4643774a3402b4e153?placeholderIfAbsent=true&apiKey=17e6b1f5aff14ccdbcafd5a66c4951ca"
-          class="aspect-square w-5 object-contain"
-          alt="Event icon"
+          class="aspect-square w-5 object-contain "
+          alt="Location icon"
           width={500}
           height={500}
         />
       </header>
       <div class="mt-2.5 flex w-full flex-col font-medium text-neutral-400">
-        <h2 class="text-sm text-gray-950">{props.title}</h2>
+        <h2 class="text-sm text-gray-950">{props.name}</h2>
         <p class="mt-1.5 text-xs leading-none">{props.description}</p>
-        <time class="mt-1.5 text-ellipsis text-xs uppercase leading-none tracking-wide">
-          {props.time}
-        </time>
+        <address class="mt-1.5 text-ellipsis text-xs uppercase not-italic leading-none tracking-wide">
+          {props.address}
+        </address>
       </div>
       <div class="mt-2.5 flex w-full items-start gap-2.5 text-center text-xs font-medium leading-none text-gray-600">
         {props.tags &&
           props.tags.map((tag, index) => <Tag key={index} text={tag} />)}
       </div>
       <div class="flex flex-row">
-        <AttendeeList
+        {/*
+
+        <RatingDisplay
           link={props.link}
-          attendees={props.attendees}
+          rating={props.rating}
           images={imgs}
         />
+
+
+        */}
+      </div>
+      <div class="flex justify-start">
+        <button class="mt-2 w-fit rounded-full bg-black px-2 py-2 font-medium text-white transition-colors hover:bg-gray-800">
+          Select Location
+        </button>
       </div>
     </article>
   );
--- src/routes/(app)/home/index.tsx
+++ src/routes/(app)/home/index.tsx
@@ -4,7 +4,9 @@ import { EventCard } from "@/components/app/eventCard/EventCard";
 import type { popupsData } from "~/models/map";
 import { routeLoader$, routeAction$ } from "@builder.io/qwik-city";
 import { QueryEvents } from "~/api/Query";
+import { LocationCard } from "@/components/app/LocationCard/LocationCard";
 export type Events = Awaited<ReturnType<typeof useEvents>>;
+
 export const head = {
   title: " S&H | Home",
 };
@@ -39,8 +41,6 @@ export const useMapData = routeLoader$(async () => {
 export default component$(() => {
   const events = useEvents();
   const coords = useMapData();
-  const intrests = ["plant", "code", "working"];
-  const location = ["montreal", "toronto", "vancouver"];
 
   return (
     <div class=" flex h-full flex-col justify-start   md:pb-12  ">
@@ -54,19 +54,6 @@ export default component$(() => {
       <div class="flex flex-col gap-5 md:gap-2 lg:grid lg:grid-cols-5">
         <div class="order-2 row-span-1 h-fit rounded-full px-2 lg:order-1 lg:col-span-3 lg:pl-2">
           <Leaflet popups={coords} />
-          <div class="pb-2 pt-4">
-            <h1 class="text-2xl font-medium">Intrests Filters</h1>
-          </div>
-          <div class="flex gap-2 md:px-0">
-            {intrests.map((intrest) => (
-              <button
-                key={intrest}
-                class="max-w-lg self-end rounded-md border bg-blue-50 p-2 text-black shadow-sm"
-              >
-                {intrest}
-              </button>
-            ))}
-          </div>
         </div>
 
         <div class="order-1 row-span-1 rounded-xl lg:order-2 lg:col-span-2">
@@ -88,35 +75,14 @@ export default component$(() => {
       <div class=" order-last px-2 pt-2">
         <h1 class="  pb-2 text-2xl  font-medium ">Good locations</h1>
         <div class=" flex  w-full flex-col content-center gap-2  md:flex-row lg:gap-4 ">
-          {location.map((location) => (
-            <div
-              key={location}
-              class="flex flex-col   gap-2 bg-gray-100 bg-opacity-30  md:w-[25em]  "
-            >
-              <div class="flex w-fit flex-col justify-center  gap-2 rounded-lg   border p-0    align-middle md:w-full">
-                <img
-                  src="https://s3.ca-central-1.amazonaws.com/files.quartierdesspectacles.com/lieux/esplanade-tranquille/2022/printemps/esplanade-tranquille-pqds-2022-esplanade-tranquille-printemps-ete-c-vivien-gaumand-20-390x260.jpg"
-                  alt="location"
-                  width={400}
-                  height={100}
-                  class="rounded-md rounded-b-none  "
-                />
-                <div class="gap-2  ">
-                  <div class="px-2 pb-2">
-                    <p class="text-md  pb-1  font-thin text-black  ">
-                      <span class="font-medium"> {location} </span> open 24/7
-                    </p>
-                    <p class=" text-md font-thin  text-black ">
-                      a cozy spot where people dont bother your for coffee{" "}
-                    </p>
-                  </div>
-                  <button class=" max-w-lg cursor-pointer self-start rounded-md   bg-gray-50   p-3 text-black  shadow-md ">
-                    book now
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
+          <LocationCard
+            name="Esplanda"
+            link="/montreal"
+            tags={["festival", "city"]}
+            rating={5}
+            address="Montreal, Quebec, Canada"
+            description="The city of festivals"
+          />
         </div>
       </div>
     </div>
