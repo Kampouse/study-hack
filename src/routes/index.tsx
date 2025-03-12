83519446db9cd50a6b9f13dce2997ca4d96dd38d
jean-philippe martel <jpmartel98@gmail.com>
2025-03-12 16:33:03 -0400

    added image thing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,59 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import OG from "/just-rnd.png";
 import Landing from "./landing";
 export default component$(() => {
   return <Landing />;
 });
 export const head: DocumentHead = {
-  title: "Just RND",
+  title: "Just RND | Collaborative Study Platform",
   meta: [
     {
       name: "description",
-      content: "some plaform about collaboration",
+      content:
+        "Find study buddies nearby and collaborate with fellow students at different places in your city. Join study sessions and build connections.",
+    },
+    {
+      name: "og:title",
+      content: "Study Hack | Collaborative Study Platform",
+    },
+    {
+      name: "og:description",
+      content:
+        "Find study buddies nearby and collaborate with fellow students at different places in your city. Join study sessions and build connections.",
+    },
+    {
+      name: "og:image",
+      content: OG,
+    },
+    {
+      name: "og:image:width",
+      content: "1200",
+    },
+    {
+      name: "og:image:height",
+      content: "630",
+    },
+    {
+      name: "og:url",
+      content: "https://www.justrnd.com",
+    },
+    {
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      name: "twitter:title",
+      content: "Just RND | Collaborative Study Platform",
+    },
+    {
+      name: "twitter:description",
+      content:
+        "Find study buddies nearby and collaborate with fellow students at different places in your city. Join study sessions and build connections.",
+    },
+    {
+      name: "twitter:image",
+      content: OG,
     },
   ],
 };
