e488f716286796063f8caa5ae74b2ca1cbd702ba
jean-philippe <jp@zelexio.com>
2025-04-05 18:07:49 -0400

    stuff

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -8,12 +8,12 @@ import {
 export default component$(() => {
   return (
     // Keep the main background color, the warmth is part of the theme
-    <div class="min-h-screen bg-[#FFF8F0] pb-16 md:pb-20">
+    <div class="min-h-screen bg-[#FFF8F0] py-32 pb-16 md:pb-20">
 
 
 
 
-      <h1 class="text-2xl font-bold">Profile</h1>
+      <h1 class="text-2xl bg-red-500 font-bold text-center">Profile</h1>
 
 
     </div>
