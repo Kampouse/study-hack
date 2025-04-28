8294fe73bb55b3dc4da3a4f0a57e44d0a4eaa2a3
jean-philippe martel <jpmartel98@gmail.com>
2025-04-28 19:44:14 -0400

    worky

--- src/components/profile/TabsSection.tsx
+++ src/components/profile/TabsSection.tsx
@@ -1,4 +1,4 @@
-import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
 import { Link } from "@builder.io/qwik-city";
 import {
