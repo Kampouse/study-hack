bb3a0d12433560a77d6a73caa113fbf15b8bdffb
jean-philippe martel <jpmartel98@gmail.com>
2025-06-01 16:11:53 -0400

    fixed place thing

--- src/routes/places/[name]/index.tsx
+++ src/routes/places/[name]/index.tsx
@@ -247,7 +247,7 @@ export default component$(() => {
                 src={(place.value.data?.ImageURL as string) || ""}
                 alt={place.value.data?.Name}
                 class="h-[250px] w-full object-cover sm:h-[350px] md:h-[450px]"
-                width={900}
+                width={824}
                 height={450}
               />
               <div class="absolute inset-0 bg-gradient-to-t from-black/40 via-transparent to-transparent" />
