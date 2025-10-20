35803e05b340f2b83dee13d924a173c7371224d0
jean-philippe martel <jpmartel98@gmail.com>
2025-10-20 00:11:28 -0400

    removed the  vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import eslintPlugin from "@nabla/vite-plugin-eslint";
+
 import pkg from "./package.json";
 
 type PkgDep = Record<string, string>;
